c43aa84a423760d9df7f66b119cb1f3858444e5e
Patrick Lima <patrickgdlima@gmail.com>
2021-09-27 00:24:00 -0300

    refactor(input): fix folder structure

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { InputModule } from '@features/input/input.module';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule]
+  imports: [CommonModule, LoginRoutingModule, InputModule]
 })
 export class LoginModule {}
