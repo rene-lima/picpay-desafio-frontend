2626cef906d1bb720e262b878c2c2ed37071512d
Patrick Lima <patrickgdlima@gmail.com>
2021-09-27 00:25:12 -0300

    feat(button): shared component

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html'
+})
+export class ButtonComponent {
+  @Input() buttonLabel: string;
+  @Input() buttonType = 'button';
+
+  @Output() clicked = new EventEmitter();
+
+  constructor() {}
+
+  onClick() {}
+}
