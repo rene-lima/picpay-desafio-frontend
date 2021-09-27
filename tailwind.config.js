a0c79233c76748fd2056b5f49ec0ae6711042fad
Patrick Lima <patrickgdlima@gmail.com>
2021-09-27 00:01:17 -0300

    feat(assets): font and svgs used on project
    Montserrat, Roboto and Lato fonts. SVGs illustrations.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,64 @@
 module.exports = {
-  // mode: 'jit',
+  mode: 'jit',
   purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      heading: [
+        'Montserrat',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      body: [
+        'Roboto',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      display: [
+        'Lato',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ]
+    },
+    extend: {}
   },
   variants: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/forms')
-],
+    extend: {}
+  }
 };
