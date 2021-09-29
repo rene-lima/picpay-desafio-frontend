0d8458f1df1dd3468776209772eb00e1ceb7700a
Patrick Lima <patrickgdlima@gmail.com>
2021-09-29 17:18:09 -0300

    test(api): service unit testing

--- src/app/services/api/api.service.spec.ts
+++ src/app/services/api/api.service.spec.ts
@@ -1,16 +1,62 @@
+import { HttpParams } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { environment } from '@environment/environment';
 
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
-  let service: ApiService;
+  let apiService: ApiService;
+  let httpTesting: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    apiService = TestBed.inject(ApiService);
+    httpTesting = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(apiService).toBeTruthy();
+  });
+
+  it('should httpClient get method', () => {
+    apiService.get('endpoint').subscribe();
+    const req = httpTesting.expectOne(`${environment.API_URL}endpoint`);
+    expect(req.request.method).toEqual('GET');
+  });
+
+  it('should invoke httpClient post method', () => {
+    const obj = { foo: 'foo', bar: 'bar' };
+
+    apiService.post('endpoint', obj).subscribe();
+    const req = httpTesting.expectOne(`${environment.API_URL}endpoint`);
+    expect(req.request.body).toEqual(obj);
+    expect(req.request.method).toEqual('POST');
+  });
+
+  it('should invoke httpClient put method', () => {
+    const mock = { foo: 'foo', bar: 'bar' };
+
+    apiService.put('endpoint/1', mock).subscribe();
+    const req = httpTesting.expectOne(`${environment.API_URL}endpoint/1`);
+    expect(req.request.body).toEqual(mock);
+    expect(req.request.method).toEqual('PUT');
+  });
+
+  it('should invoke httpClient patch method', () => {
+    const mock = { foo: 'foo' };
+
+    apiService.patch('endpoint/1', mock).subscribe();
+    const req = httpTesting.expectOne(`${environment.API_URL}endpoint/1`);
+    expect(req.request.body).toEqual(mock);
+    expect(req.request.method).toEqual('PATCH');
+  });
+
+  it('should invoke httpClient delete method', () => {
+    apiService.delete('endpoint/1').subscribe();
+    const req = httpTesting.expectOne(`${environment.API_URL}endpoint/1`);
+    expect(req.request.method).toEqual('DELETE');
   });
 });
