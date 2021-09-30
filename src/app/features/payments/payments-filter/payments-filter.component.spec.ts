cdcf19093d7269a4443ef09cc78d5bd32cb2df93
Patrick Lima <patrickgdlima@gmail.com>
2021-09-29 21:52:13 -0300

    test(payments-filter): unit testing

--- src/app/features/payments/payments-filter/payments-filter.component.spec.ts
+++ src/app/features/payments/payments-filter/payments-filter.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PaymentsFilterComponent } from './payments-filter.component';
 
 describe('PaymentsFilterComponent', () => {
@@ -20,5 +19,26 @@ describe('PaymentsFilterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should include a search text, trigger button and emit value searched', () => {
+    const nameToSearch = 'Patrick';
+    // spy on event emitter
+    const submittedSpy = spyOn(component.filtered, 'emit');
+
+    const searchInput: HTMLInputElement = fixture.nativeElement.querySelector('input');
+
+    // simulate user entering a new search into the input box
+    searchInput.value = nameToSearch;
+
+    // Dispatch a DOM event so that Angular learns of input value change.
+    searchInput.dispatchEvent(new Event('input'));
+
+    // trigger the click
+    fixture.nativeElement.querySelector('#filter').click();
+    fixture.detectChanges();
+
+    expect(submittedSpy).toHaveBeenCalled();
+    expect(submittedSpy).toHaveBeenCalledWith(nameToSearch);
   });
 });
