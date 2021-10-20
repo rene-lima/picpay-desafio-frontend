0945443073389266c404976df4e11185850354f4
Felipe Xavier <felipe.xavier@tivit.com>
2021-10-19 22:29:46 -0300

    Iniciando projeto: criando estrutura de modulos e servicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PaymentComponent } from './modules/payment/payment.component';
+import { LoginComponent } from './modules/login/login.component';
 @NgModule({
   declarations: [	
-    AppComponent,
+    AppComponent, LoginComponent, PaymentComponent,
    ],
   imports: [
     BrowserModule
