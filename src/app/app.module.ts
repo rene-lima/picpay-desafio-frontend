0c5b4e8f06116decc5e7857493e47f619dfe76e8
Kauã Semenov <kaua.semenov@gmail.com>
2021-10-20 23:23:09 -0300

    Feat: add login page layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [	
-    AppComponent,
+    AppComponent, LoginComponent,
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
