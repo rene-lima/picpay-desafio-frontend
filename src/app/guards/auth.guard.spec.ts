971100b4634525276a4bbbb5ff76968a6e7fbb45
Kauã Semenov <kaua.semenov@gmail.com>
2021-10-25 00:19:31 -0300

    Test: Add tests in Auth Guard

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,22 +1,46 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { AuthService } from '../services/auth/auth.service';
 
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
   let service: AuthGuard;
-  let routerSpy: { get: jasmine.Spy };
+  let authServiceSpy: jasmine.SpyObj<AuthService>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
+    authServiceSpy = jasmine.createSpyObj('AuthService', ['userIsAuthenticated']);
     routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
     TestBed.configureTestingModule({
-      providers: [AuthGuard, { provide: Router, useValue: routerSpy }],
+      providers: [
+        AuthGuard,
+        { provide: Router, useValue: routerSpy },
+        { provide: AuthService, useValue: authServiceSpy },
+      ],
     });
-    service = TestBed.inject(AuthGuard);
+    service = new AuthGuard(authServiceSpy, routerSpy);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return true when call userIsAuthenticated', () => {
+    authServiceSpy.userIsAuthenticated.and.returnValue(true)
+
+    service.canActivate(null, null);
+
+    expect(authServiceSpy.userIsAuthenticated()).toBeTrue()
+  });
+
+  it('should return false when call userIsAuthenticated', () => {
+    authServiceSpy.userIsAuthenticated.and.returnValue(false)
+
+    service.canActivate(null, null);
+
+    expect(authServiceSpy.userIsAuthenticated()).toBeFalse()
   });
 });
