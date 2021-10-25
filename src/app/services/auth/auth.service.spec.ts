8d075617fb3079536ef7a4b1ad6139cc274a0e86
Kauã Semenov <kaua.semenov@gmail.com>
2021-10-24 23:27:44 -0300

    Test: Add tests in Auth Service

--- src/app/services/auth/auth.service.spec.ts
+++ src/app/services/auth/auth.service.spec.ts
@@ -7,21 +7,48 @@ import { AuthService } from './auth.service';
 describe('AuthService', () => {
   let service: AuthService;
   let httpClientSpy: { get: jasmine.Spy };
-  let routerSpy: { get: jasmine.Spy };
+  let router: jasmine.SpyObj<Router>
+
+  const user = {
+    id: 0,
+    name: 'usuario',
+    email: 'usuario@gmail.com',
+    password: 'usuario',
+  };
 
   beforeEach(() => {
-    httpClientSpy = jasmine.createSpyObj("HttpClient", ["get"]);
-    routerSpy = jasmine.createSpyObj("Router", ["navigate"]);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    router = jasmine.createSpyObj<Router>(['navigate'])
 
-    TestBed.configureTestingModule({ providers: [
-      AuthService,
-      { provide: HttpClient, useValue: httpClientSpy },
-      { provide: Router, useValue: routerSpy}
-    ]});
+
+    TestBed.configureTestingModule({
+      providers: [
+        AuthService,
+        { provide: HttpClient, useValue: httpClientSpy },
+        { provide: Router, useValue: router },
+      ],
+    });
     service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call authUser()', () => {
+    let authUserSpy = spyOn(service, "authUser")
+    authUserSpy(user, user);
+    expect(authUserSpy).toHaveBeenCalled();
+  });
+
+  it('should call router navigate', () => {
+    service.authUser(user, user)
+    expect(router.navigate).toHaveBeenCalled()
+  })
+
+  it('should call userIsAuthenticated()', () => {
+    let userIsAuthenticatedSpy = spyOn(service, "userIsAuthenticated");
+    userIsAuthenticatedSpy();
+    expect(userIsAuthenticatedSpy).toHaveBeenCalled();
   });
 });
