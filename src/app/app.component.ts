70bf5cba037c5cd877f78132cc4f4501e278f27b
Felipe Xavier <felipe.xavier@tivit.com>
2021-10-25 21:10:34 -0300

    Implementação do css do login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  title: string;
-
-  ngOnInit() {
-   this.title = 'Desafio Picpay Front-end';
-  }
-}
+export class AppComponent {}
--- src/app/modules/login/components/login.component.ts
+++ src/app/modules/login/components/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { User } from 'src/app/shared/models/login/user.model';
 import { LoginService } from 'src/app/shared/services/login/login.service';
 
@@ -10,23 +11,42 @@ import { LoginService } from 'src/app/shared/services/login/login.service';
 export class LoginComponent implements OnInit {
 
   public user: User;
+  public loadingLogin: boolean = false;
 
-  constructor(private loginService: LoginService) {
+  constructor(
+    private loginService: LoginService, 
+    public snackBar: MatSnackBar
+  ) {
     this.user = new User();
   }
 
   ngOnInit(): void {
   }
 
-  public login() {
+  public login() {    
 
-    console.log(this.user);
+    if (!this.verifyEmail()) {
+      return;
+    }
+
+    if (this.user.password == undefined || this.user.password == '') {
+      console.log("Por favor, preencher os campos usuário e senha!");
+      return;
+    }
+
+    this.loadingLogin = true;
 
     this.loginService.login(this.user).subscribe((userResponse: User) => {
       if (Object.keys(userResponse).length === 0) {
         console.log('Usuário ou senha incorreto!');
         return;
       }
+      this.loadingLogin = false;
     });
+  }
+
+  public verifyEmail(): boolean {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(this.user.email);
   }
 }
