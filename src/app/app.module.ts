ff84c7c04b92580ba6b000836b15e8552ba53cfc
SamanthaAquino <samantha_aquino@outlook.com>
2021-10-27 18:38:08 -0300

    Ajustes configurações desafio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
-   ],
+  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
