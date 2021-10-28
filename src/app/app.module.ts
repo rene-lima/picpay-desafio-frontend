2c806edbd6ecf0650ca49dfab7569aba4b16de91
SamanthaAquino <samantha_aquino@outlook.com>
2021-10-28 20:12:07 -0300

    Pagina Login - Criação e ajustes necessários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+
+import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './header/header.component';
+import { PaymentsComponent } from './payments/payments.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    PaymentsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    HttpClientModule,
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
