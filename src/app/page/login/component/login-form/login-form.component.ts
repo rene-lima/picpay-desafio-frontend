f8cb7d4632c11fcee55281e885e7d86af0c25231
Luiz R Carvalho <beto_carvalho@hotmai.com>
2021-10-29 01:27:55 -0300

    style(dto): mudanca de pasta dos DTO's
    foi feita essa mudanca por fazer mais sentido os DTO's ficarem dentro da pasta app

--- src/app/page/login/component/login-form/login-form.component.ts
+++ src/app/page/login/component/login-form/login-form.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {LoginDTO} from '../../../../../core/DTO/loginDTO';
+import {LoginDTO} from '../../../../core/DTO/loginDTO';
 
 
 
--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginDTO } from '../../../core/DTO/loginDTO';
+import { LoginDTO } from '../../core/DTO/loginDTO';
 
 @Component({
   selector: 'app-login',
